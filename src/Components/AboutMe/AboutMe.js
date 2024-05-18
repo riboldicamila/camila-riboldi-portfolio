294fc89a8036d6963ed2793688d25b9973b79515
Camila Riboldi <cami.riboldi@gmail.com>
2024-05-18 20:57:44 -0300

    update data

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -11,13 +11,22 @@ function AboutMe() {
     <div className="card-container">
       <HomeCard
         image={university}
-        title="Bachelors Degree"
-        text="bkakabakabka"
+        title="Bachelor in Information Systems | 5 ½ years program"
+        text="The Bachelor in Information Systems program is a comprehensive journey merging business management with technological innovation. 
+        The curriculum is divided into distinct phases. The initial two years focus on economics, finance, and management principles, providing a holistic understanding of the business landscape.
+        Later stages emphasize Information Technology (IT) management. Learning about IT strategy, project management, and programming foundations."
       />
       <HomeCard
         image={languages}
         title="Courses and Trainnings"
-        text="bkakabakabka"
+        text="
+        Advanced React - Coursera by Meta
+        React Js 35hs - Coderhouse
+        Web Development | html-css-saas 38hs - Coderhouse
+        UX Design: From Concept to Prototype - University of Michigan
+        Technical Support Fundamentals - Coursera
+        Marketing Fundamentals 30hs - Capacitarte
+        Coding Bootcamp: OOP Fundamentals based on TDD (test driven development)"
       />
       <HomeCard
         image={courses}
