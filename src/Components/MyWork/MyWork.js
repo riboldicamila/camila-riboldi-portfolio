0bc2ab7bf7a5e25ace43e590203940e8560e63cd
Camila Riboldi <cami.riboldi@gmail.com>
2024-05-25 14:50:14 -0300

    my work tab changes

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,37 +1,73 @@
 import React from "react";
-import "./MyWork.css";
 import MyWorkCard from "./MyWorkCard";
-
-import university from "../AboutMe/uba-photo.jpg";
 import TextCard from "./TextCard";
+
+import swissMedia from "./swissMedia-html.png";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import Diversity2Icon from "@mui/icons-material/Diversity2";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import BrokenImageIcon from "@mui/icons-material/BrokenImage";
+
+import "./MyWork.css";
 
 function MyWork() {
   return (
     <div className="my-work-tab">
       <div className="banner">
-        <h2>Lets get creative!</h2>
+        <h2>Check out some of my work!</h2>
       </div>
 
       <div className="knowledge-cards">
-        <TextCard />
-        <TextCard />
-        <TextCard />
-        <TextCard />
+        <TextCard
+          icon={Diversity2Icon}
+          title={"Teamwork is Essential"}
+          text={
+            "My bachelor's degree gave me a solid grasp of how organizations work and why teamwork matters."
+          }
+        />
+        <TextCard
+          icon={TravelExploreIcon}
+          title={"Don't Limit Yourself"}
+          text={
+            "Technology is always evolving. I believe in finding what you love but also staying open to new technologies."
+          }
+        />
+        <TextCard
+          icon={AutoAwesomeIcon}
+          title={"Listening is Key"}
+          text={
+            "Don't build something unnecessary. Understand the real needs first."
+          }
+        />
+        <TextCard
+          icon={BrokenImageIcon}
+          title={"Design and Usability"}
+          text={" Know your user, try to break things, then fix them."}
+        />
       </div>
 
       <div className="card-block">
         <MyWorkCard
-          image={university}
+          image={swissMedia}
+          title="SwissMedia HTML || CSS"
+          text="In this project, you'll find a website created with HTML and CSS, featuring experiments with Sass, Flexbox, and general frontend concepts.
+          To develop this site, I started with wireframes and thoughtfully designed the layout of each tab and page. The final product is deployed and available at GitHub Pages.
+          The project uses several libraries and technologies, including:
+          HTML
+          CSS
+          Sass
+          Bootstrap
+          Google Fonts
+          Animate.css
+          Explore the various features and styles incorporated in this project!"
+        />
+        <MyWorkCard
+          image={swissMedia}
           title="SwissMedia"
           text="dfjsljkgljdsjhds"
         />
         <MyWorkCard
-          image={university}
-          title="SwissMedia"
-          text="dfjsljkgljdsjhds"
-        />
-        <MyWorkCard
-          image={university}
+          image={swissMedia}
           title="SwissMedia"
           text="dfjsljkgljdsjhds"
         />
--- src/Components/MyWork/TextCard.js
+++ src/Components/MyWork/TextCard.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './TextCard.css'; // Importing the CSS file for styling
+import React from "react";
+import "./TextCard.css"; // Importing the CSS file for styling
 
-const TextCard = () => {
+const TextCard = ({ icon: Icon, title, text }) => {
   return (
     <div className="card">
-      <div className="card-icon">Icon</div>
-      <div className="card-title">Title</div>
-      <div className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+
+      <div className="card-icon"> <Icon /></div>
+      <div className="card-title">{title}</div>
+      <div className="card-text">{text}</div>
     </div>
   );
 };
