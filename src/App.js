fd0ca7879bf0e9d4a2be0f108bf7e6d7190caed1
Camila Riboldi <cami.riboldi@gmail.com>
2024-06-01 17:18:49 -0300

    work in progress..

--- src/App.js
+++ src/App.js
@@ -6,23 +6,39 @@ import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import AboutMe from "./Components/AboutMe/AboutMe";
 import MyWork from "./Components/MyWork/MyWork";
-
+import WorkInProgress from "./Components/WorkInProgress/WorkInProgress";
 import "./App.css";
 import "animate.css/animate.min.css";
-import 'balloon-css';
+import "balloon-css";
 
 
 function App() {
+  const isMobile = window.innerWidth < 768;
+
+  function isPageUnderConstruction() {
+    const underConstructionRoutes = ["/my-work"]; 
+    const currentRoute = window.location.pathname; 
+    
+    return underConstructionRoutes.includes(currentRoute);
+  }
+  
+
+  if (isMobile || isPageUnderConstruction()) {
+    return <WorkInProgress />;
+  }
+
   return (
-    <Router>
+    <div>
+      <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/my-work" element={<MyWork />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/my-work" element={<MyWork />} />
+        </Routes>
       <Footer />
     </Router>
+    </div>
   );
 }
 
