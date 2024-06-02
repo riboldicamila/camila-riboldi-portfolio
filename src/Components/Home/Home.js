384ef02ccb43c0c6df6c93c4634c7bcb09a9716e
Camila Riboldi <cami.riboldi@gmail.com>
2024-06-02 13:20:40 -0300

    style fixes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useRef } from "react";
 
 import "./Home.css";
 import homePicture from "./home-picture-7.jpg";
@@ -23,6 +23,7 @@ function Home() {
     threshold: 0.8,
   });
 
+
   return (
     <div className="home">
       <div className="image-with-text-overlay">
@@ -43,9 +44,9 @@ function Home() {
         <p>
           I'm a software developer with a focus on frontend, curious about UX
           and UI. <br /> I have some knowledge of backend and databases, and I
-          hold a bachelor's degree  <br /> in Information Systems. I enjoy teamwork and
-          am committed to creating <br /> user-friendly experiences while   continuously
-          learning and expanding my skills.
+          hold a bachelor's degree <br /> in Information Systems. I enjoy
+          teamwork and am committed to creating <br /> user-friendly experiences
+          while continuously learning and expanding my skills.
         </p>
 
         <PrimaryButton href="none" className="home-btn" text="Know More" />
@@ -54,12 +55,15 @@ function Home() {
         className={`banner-technologies ${isIntersecting ? "" : "hidden"}`}
         ref={bannerRef}
       >
-        <img src={scrum} alt="Background" className="icon-image" />
-        <img src={git} alt="Background" className="icon-image" />
-        <img src={css} alt="Background" className="icon-image" />
-        <img src={html} alt="Background" className="icon-image" />
-        <img src={js} alt="Background" className="icon-image" />
-        <img src={react} alt="Background" className="icon-image" />
+        <div className="home-carrousel">
+          <img src={scrum} alt="Background" className="icon-image" />
+          <img src={git} alt="Background" className="icon-image" />
+          <img src={css} alt="Background" className="icon-image" />
+          <img src={html} alt="Background" className="icon-image" />
+          <img src={js} alt="Background" className="icon-image" />
+          <img src={react} alt="Background" className="icon-image" />
+     
+        </div>
       </div>
     </div>
   );
